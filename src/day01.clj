201b1ce78ec5de9711086ea0c0a511d37ae7ba82
Felix Koch <fkoch@student.tgm.ac.at>
2023-12-09 22:52:18 +0100

    day 01.2

--- src/day01.clj
+++ src/day01.clj
@@ -16,15 +16,41 @@
   (filter is-digit (seq s))
   )
 
-(defn calib-value [^String s]
+(defn calib-value [^String s digit-conversion-function]
   "return the calibration value for s"
   (Integer/parseInt
-    (let [dgs (only-digits s)]
+    (let [dgs (digit-conversion-function s)]
       (str
         (first dgs)
         (last dgs)
         ))
     )
+  )
+
+(def digit-map {
+                "one"   1
+                "two"   2
+                "three" 3
+                "four"  4
+                "five"  5
+                "six"   6
+                "seven" 7
+                "eight" 8
+                "nine"  9
+                }
+  )
+
+(def reversed-digit-map {
+                         "xis"   6
+                         "eerht" 3
+                         "owt"   2
+                         "neves" 7
+                         "evif"  5
+                         "thgie" 8
+                         "eno"   1
+                         "enin"  9
+                         "ruof"  4
+                         }
   )
 
 
@@ -33,11 +59,40 @@
   (with-open
     [xin (clojure.java.io/reader (clojure.java.io/resource ifile))]
     (reduce +
-            (map calib-value
+            (map #(calib-value % only-digits)
                  (line-seq xin)
                  )
             )
     )
   )
 
-(println (str "Part One: " (part-one)))
+(defn int-ify-first-substring [line digit-map]
+  (let [pattern (re-pattern (str "\\d|" (clojure.string/join "|" (keys digit-map))))]
+    (let [match (re-find pattern line)]
+      (if (= (count match) 1)
+        (Integer/parseInt match)
+        (digit-map match)
+        )
+      )
+    )
+  )
+
+(defn part-two []
+  "Part two of day one"
+  (with-open
+    [xin (clojure.java.io/reader (clojure.java.io/resource ifile))]
+    (reduce +
+            (for [line (line-seq xin)]
+              (Integer/parseInt
+                (str
+                  (int-ify-first-substring line digit-map)
+                  (int-ify-first-substring (clojure.string/reverse line) reversed-digit-map)
+                  )
+                )
+              )
+            )
+    )
+  )
+
+;(println (str "Part One: " (part-one)))
+(println (str "Part Two: " (part-two)))
