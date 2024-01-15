40c9902a592c801aab28b51ffc91ee627b4fea62
Felix Koch <felixkoch.it@gmail.com>
2024-01-15 13:54:00 +0100

    wip

--- src/day03.clj
+++ src/day03.clj
@@ -1,6 +1,6 @@
 (ns day03
-  (:require [clojure.string :as str]
-            [clojure.core.matrix :refer :all]))
+  (:require [clojure.string :as str])
+  (:use [clojure.core.matrix]))
 
 (def ifile (clojure.java.io/resource (str "day03/" "test1")))
 ;(def ifile (clojure.java.io/resource (str "day03/" "input")))
@@ -22,11 +22,43 @@
 (defn get
   "Get the value at index (specified by a vector)"
   [matrix index]
-  (select matrix (index 0) (index 1))
+  ((matrix (index 1)) (index 0))
   )
-(for [index (index-seq file-matrix)]
-  (do
-    (println (str "i: " index))
-    (println (str "v: " (get file-matrix index)))
+
+(defn is-symbol?
+  [^Character c]
+  (and
+    (not (= c \.))
+    (not (Character/isDigit c))
+    ))
+(defn is-digit?
+  [index]
+  (Character/isDigit (get file-matrix index))
+  )
+
+(defn get-symbol-keys []
+  (filter #(is-symbol? (get file-matrix %)) (index-seq file-matrix)))
+
+(defn get-sur-digit-keys [key excluded-keys]
+  (let [excluded-keys (conj excluded-keys key)
+        ; all wrong : Zahlen sind nur in einer Zeile
+        offsets (for [x [-1 0 1] y [-1 0 1]] [x y])
+        all-keys (mapv #(mapv + %1 %2) offsets (repeat key))
+        check-keys (filter #(not (contains? excluded-keys %)) all-keys)
+        ]
+    (filter is-digit? check-keys)
     )
   )
+
+(defn f [start-keys]
+  (loop [start-keys start-keys excluded-keys []]
+    (let [new-keys (get-sur-digit-keys )])
+    )
+  )
+
+;(for [index (index-seq file-matrix)]
+;  (do
+;    (println (str "i: " index))
+;    (println (str "v: " (get file-matrix index)))
+;    )
+;  )
