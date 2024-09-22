ff11f7d4bcc514e598f3e6c3a7563367f95499ad
Felix Koch <felixkoch.it@gmail.com>
2024-09-22 13:46:08 +0200

    state laptop

--- src/day03.clj
+++ src/day03.clj
@@ -22,43 +22,40 @@
 (defn get
   "Get the value at index (specified by a vector)"
   [matrix index]
-  select (matrix (index 0) (index 1))
+  ((matrix (index 1)) (index 0))
   )
 
-(defn is-symbol?
+(defn symbol?
   [^Character c]
   (and
     (not (= c \.))
     (not (Character/isDigit c))
     ))
-(defn is-digit?
+
+(defn mask-index
   [index]
-  (Character/isDigit (get file-matrix index))
+  (for [dx [-1 0 1] dy [-1 0 1]] [(- (index 0) dx) (- (index 1) dy)])
   )
 
-(defn get-symbol-keys []
-  (filter #(is-symbol? (get file-matrix %)) (index-seq file-matrix)))
-
-(defn get-sur-digit-keys [key excluded-keys]
-  (let [excluded-keys (conj excluded-keys key)
-        ; all wrong : Zahlen sind nur in einer Zeile
-        offsets (for [x [-1 0 1] y [-1 0 1]] [x y])
-        all-keys (mapv #(mapv + %1 %2) offsets (repeat key))
-        check-keys (filter #(not (contains? excluded-keys %)) all-keys)
-        ]
-    (filter is-digit? check-keys)
+(defn mark-adjecent-numbers
+  [x y]
+  (let* [matrix file-matrix
+         curr-char (get matrix [x y])]
+    (cond
+      (symbol? curr-char) (mask-index [x y])
+      :else #{})
     )
   )
 
-(defn f [start-keys]
-  (loop [start-keys start-keys excluded-keys []]
-    (let [new-keys (get-sur-digit-keys )])
-    )
-  )
-
-;(for [index (index-seq file-matrix)]
-;  (do
-;    (println (str "i: " index))
-;    (println (str "v: " (get file-matrix index)))
-;    )
-;  )
+(loop [
+       s #{}
+       indices (for [x (range ((shape file-matrix) 0)) y (range ((shape file-matrix) 1))] [x y])
+       ]
+  (let* [
+         curr-idx (peek indices)
+         x (curr-idx 0)
+         y (curr-idx 1)
+         marks (mark-adjecent-numbers x y)
+         new-set (apply conj s marks)]
+    (recur new-set (pop indices))
+    ))
